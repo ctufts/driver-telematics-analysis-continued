00fb59f57109baafdb0dc4c4c21657b01e83d119
ctufts <christopher.tufts25@gmail.com>
2015-02-23 23:18:47 -0500

    add doMC version of histogram gbm evaluation

---
+++ gbm_speedhistogram_doMC.R
@@ -0,0 +1,70 @@
+rm(list = ls())
+library(doMC)
+library(foreach)
+library(gbm)
+
+speedDistributionHistogram <- function(trip)
+{
+  min.speed <- 0
+  max.speed <- 160
+  bin.width <- 8
+  speed <- 3.6*sqrt(diff(trip$x,20,1)^2 + diff(trip$y,20,1)^2)/20
+  speed.hist <- hist(speed[speed <= max.speed & speed >= min.speed],
+                plot = F, breaks= seq(min.speed, max.speed, bin.width))$count
+  return(speed.hist/sum(speed.hist))
+}
+
+set.seed(25)
+drivers = list.files("drivers")
+randomDrivers = sample(drivers, size = 5)
+
+ref.data = NULL
+target = 0
+names(target) = "target"
+# 
+registerDoMC(4)
+
+refData <- foreach(driver = iter(randomDrivers), .combine = rbind)%dopar%
+{
+  dirPath = paste0("drivers/", driver, '/')
+  for(i in 1:200)
+  {
+    trip = read.csv(paste0(dirPath, i, ".csv"))
+    features = c(speedDistributionHistogram(trip), target)
+    ref.data = rbind(ref.data, features)
+  }
+  ref.data
+}
+
+
+n.trees <- 5000
+target = 1
+names(target) = "target"
+submission = NULL
+submission <- foreach(driver = iter(drivers), .combine = rbind,
+                      .packages = "gbm") %dopar%
+{
+  print(driver)
+  dirPath = paste0("drivers/", driver, '/')
+  currentData = NULL
+  for(i in 1:200)
+  {
+    trip = read.csv(paste0(dirPath, i, ".csv"))
+    features = c(speedDistributionHistogram(trip), target)
+    currentData = rbind(currentData, features)
+  }
+  train = rbind(currentData, refData)
+  train = as.data.frame(train)
+  g = gbm(target ~ ., data=train,n.trees = n.trees, distribution = "bernoulli")
+  currentData = as.data.frame(currentData)
+  p =predict(g, currentData, n.trees = n.trees, type = "response")
+  labels = sapply(1:200, function(x) paste0(driver,'_', x))
+  result = cbind(labels, p)
+  #   submission = rbind(submission, result)
+}
+
+
+
+colnames(submission) = c("driver_trip","prob")
+write.csv(submission,
+          "submissions/submission_gbm_speedhistogram.csv", row.names=F, quote=F)
